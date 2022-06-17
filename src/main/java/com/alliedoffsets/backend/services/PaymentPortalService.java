4ec7c94f543b313985d0e681d666f1d6c3d2c19e
LukeM <luke.martin@alliedcrowds.com>
2022-06-17 14:15:51 +0100

    AOW-3: Completed basic set-up and config of simple Spring Boot application. Also set up basic project directories

---
+++ src/main/java/com/alliedoffsets/backend/services/PaymentPortalService.java
@@ -0,0 +1,12 @@
+package com.alliedoffsets.backend.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPortalService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentPortalService.class, args);
+    }
+}
---
+++ src/test/java/com/alliedoffsets/test/PaymentPortalTests.java
@@ -0,0 +1,4 @@
+package com.alliedoffsets.test;
+
+public class PaymentPortalTests {
+}
